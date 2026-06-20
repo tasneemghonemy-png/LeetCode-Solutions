ac43b2d9596d4ecf966604c364c479d4b4557aef
Tasneem Ahmed <tasneemghonemy@gmail.com>
2026-06-20 16:28:39 +0300

    Time: 541 ms (52.83%), Space: 0B (100%) - LeetHub

---
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -0,0 +1,8 @@
+/* Write your PL/SQL query statement below */
+select customer_id as "customer_id"
+from customer
+group by customer_id 
+having count(distinct product_key) =  (
+                                select count( product_key) 
+                                from product
+                            ) 
