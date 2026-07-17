4ea913e437a360d464f34172f83c0b28d4385d51
Tasneem Ahmed <tasneemghonemy@gmail.com>
2026-07-17 16:51:08 +0300

    Time: 11 ms (95.97%), Space: 50.8 MB (5.5%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,5 +1,39 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer,Integer> freqMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++){
+            freqMap.put(nums[i],freqMap.getOrDefault(nums[i],0) + 1);
+        }
+
+        final int BUCKET_NUMBER = nums.length + 1;
+
+        List<List<Integer>> buckets = new ArrayList<>();
+
+        for(int i = 0; i < BUCKET_NUMBER; i++){
+            buckets.add(new ArrayList<>());
+        }
+
+        for(int num: freqMap.keySet()){
+            int bucket_index = freqMap.get(num);
+            buckets.get(bucket_index).add(num);
+        }
+
+        int[]res = new int[k];
+        int m = 0;
+        for(int i = buckets.size() - 1; i >= 0; i--){
+            List<Integer> currBucket = buckets.get(i);
+            if(!currBucket.isEmpty()){
+                for (int h = 0; m < k && h < currBucket.size(); h++, m++){
+                    res[m] = currBucket.get(h);
+                }
+            }
+        }
+        return res;
+    }
+}
+
+/*class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
         int[]res= new int[k];
         Map<Integer,Integer> map = new HashMap<>();
         for(int i = 0; i < nums.length; i++){
@@ -10,18 +44,18 @@ class Solution {
         for(int i = 0; i < k; i++){
             int maxfreq = Integer.MIN_VALUE;
             int indexOfMaxFreq = Integer.MIN_VALUE;
-           for(int  j = 0; j < nums.length;j++){
-               if(map.get(nums[j]) != null) {
-                   if(maxfreq < map.get(nums[j])){
-                       maxfreq = map.get(nums[j]);
-                       indexOfMaxFreq = j;
-                   }
+            for(int  j = 0; j < nums.length;j++){
+                if(map.get(nums[j]) != null) {
+                    if(maxfreq < map.get(nums[j])){
+                        maxfreq = map.get(nums[j]);
+                        indexOfMaxFreq = j;
+                    }
 
-               }
-           }
-           map.remove(nums[indexOfMaxFreq]);
-           res[f++] = nums[indexOfMaxFreq] ;
+                }
+            }
+            map.remove(nums[indexOfMaxFreq]);
+            res[f++] = nums[indexOfMaxFreq] ;
         }
         return res;
     }
-}
+}*/
